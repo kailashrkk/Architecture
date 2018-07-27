d6da69f5c9d9527752226eb279e7c4b5b11d0550
Kailash Ramaswamy <kailshrkk@gmail.com>
2018-07-27 19:22:54 -0400

    Initial Commit

---
+++ ArchitectureTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArchitectureTest
+//
+//  Created by Kailash on 7/27/18.
+//  Copyright © 2018 Kailash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
